ba2d4346e7b0145dac8e16849bf74c9f90c05a1a
朱炳燁 Ian <ian.chu@miniasp.com>
2018-07-14 10:52:32 +0800

    Add : ViewData, ViewBag, TempData test

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        // GET: MV
+        public ActionResult Index()
+        {
+            ViewData.Model = new {msg="hi" };
+            return View();
+        }
+
+        public ActionResult ViewDataTest()
+        {
+            ViewData["test"] = "test";
+            return View();
+        }
+
+        public ActionResult ViewBagTest()
+        {
+            ViewBag.msg = "test";
+            return View();
+        }
+        public ActionResult TempDataSave()
+        {
+            TempData["msg"] = "test";
+            return RedirectToAction("TempDataTest");
+        }
+        public ActionResult TempDataTest()
+        {
+            return View();
+        }
+    }
+}
